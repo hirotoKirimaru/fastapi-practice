b264c0a1a6585f14316bee945fc63b61241740c0
水上 皓登 <nainaistar@gmail.com>
2022-03-20 20:17:05 +0900

    chapter 10-1

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -3,7 +3,27 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-class Task(BaseModel):
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+
+class Task(TaskBase):
     id: int
     title: Optional[str] = Field(None, example="クリーニングを取りに行く")
     done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
