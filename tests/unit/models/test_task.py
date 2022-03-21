ac8438fb9dd37ea12b73d4a3a7dcdcfecb2cac0a
水上 皓登 <nainaistar@gmail.com>
2022-03-21 21:39:51 +0900

    ネスト

--- tests/unit/models/test_task.py
+++ tests/unit/models/test_task.py
@@ -7,21 +7,39 @@ import src.cruds.task
 from src.models.task import Task
 
 
-@pytest.mark.asyncio
-async def test_get_tasks_with_done_2(db):
-    # Given
-    task1 = Task(title="ダミー1")
-    task2 = Task(title="ダミー2")
-    db.add(task1)
-    db.add(task2)
+class TestGetTasksWithDone:
 
-    await db.commit()
-    await db.refresh(task1)
-    await db.refresh(task2)
+    @pytest.mark.asyncio
+    async def test_get_multiple(self, db):
+        # Given
+        task1 = Task(title="ダミー1")
+        db.add(task1)
 
-    actual = await src.cruds.task.get_tasks_with_done_2(db)
-    print("**********")
-    pprint.pprint(actual)
+        await db.commit()
+        await db.refresh(task1)
 
-    assert len(actual) == 2
-    assert actual == [(1, "ダミー1", False), (2, "ダミー2", False)]
+        # WHEN
+        actual = await src.cruds.task.get_tasks_with_done_2(db)
+
+        # THEN
+        assert len(actual) == 1
+        assert actual == [(1, "ダミー1", False)]
+
+    @pytest.mark.asyncio
+    async def test_get_multiple(self, db):
+        # Given
+        task1 = Task(title="ダミー1")
+        task2 = Task(title="ダミー2")
+        db.add(task1)
+        db.add(task2)
+
+        await db.commit()
+        await db.refresh(task1)
+        await db.refresh(task2)
+
+        # WHEN
+        actual = await src.cruds.task.get_tasks_with_done_2(db)
+
+        # THEN
+        assert len(actual) == 2
+        assert actual == [(1, "ダミー1", False), (2, "ダミー2", False)]
