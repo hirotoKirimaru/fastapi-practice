4c71b2ac1c56a889275fb41667e76d2f1661f483
水上 皓登 <nainaistar@gmail.com>
2022-03-21 21:42:08 +0900

    1件取得と2件以上取得

--- tests/unit/models/test_task.py
+++ tests/unit/models/test_task.py
@@ -6,40 +6,40 @@ from sqlalchemy.ext.asyncio import async_session, create_async_engine
 import src.cruds.task
 from src.models.task import Task
 
+class TestTask:
+    class TestGetTasksWithDone2:
 
-class TestGetTasksWithDone:
+        @pytest.mark.asyncio
+        async def test_get_single(self, db):
+            # Given
+            task1 = Task(title="ダミー1")
+            db.add(task1)
 
-    @pytest.mark.asyncio
-    async def test_get_multiple(self, db):
-        # Given
-        task1 = Task(title="ダミー1")
-        db.add(task1)
+            await db.commit()
+            await db.refresh(task1)
 
-        await db.commit()
-        await db.refresh(task1)
+            # WHEN
+            actual = await src.cruds.task.get_tasks_with_done_2(db)
 
-        # WHEN
-        actual = await src.cruds.task.get_tasks_with_done_2(db)
+            # THEN
+            assert len(actual) == 1
+            assert actual == [(1, "ダミー1", False)]
 
-        # THEN
-        assert len(actual) == 1
-        assert actual == [(1, "ダミー1", False)]
+        @pytest.mark.asyncio
+        async def test_get_multiple(self, db):
+            # Given
+            task1 = Task(title="ダミー1")
+            task2 = Task(title="ダミー2")
+            db.add(task1)
+            db.add(task2)
 
-    @pytest.mark.asyncio
-    async def test_get_multiple(self, db):
-        # Given
-        task1 = Task(title="ダミー1")
-        task2 = Task(title="ダミー2")
-        db.add(task1)
-        db.add(task2)
+            await db.commit()
+            await db.refresh(task1)
+            await db.refresh(task2)
 
-        await db.commit()
-        await db.refresh(task1)
-        await db.refresh(task2)
+            # WHEN
+            actual = await src.cruds.task.get_tasks_with_done_2(db)
 
-        # WHEN
-        actual = await src.cruds.task.get_tasks_with_done_2(db)
-
-        # THEN
-        assert len(actual) == 2
-        assert actual == [(1, "ダミー1", False), (2, "ダミー2", False)]
+            # THEN
+            assert len(actual) == 2
+            assert actual == [(1, "ダミー1", False), (2, "ダミー2", False)]
