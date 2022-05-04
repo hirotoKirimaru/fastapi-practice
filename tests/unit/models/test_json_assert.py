3f82df2a093dc747c70480363949ab375fc98f92
水上 皓登 <nainaistar@gmail.com>
2022-05-04 14:13:21 +0900

    add

--- tests/unit/models/test_json_assert.py
+++ tests/unit/models/test_json_assert.py
@@ -42,3 +42,45 @@ def test_01():
 
     assert json.loads(actual) == json.loads(expected)
     # assert actual == expected
+
+
+@pytest.mark.skipif(False, reason="ソート順番が違うのを実行する方法分からず")
+def test_02():
+    actual = """
+    {
+      "parent": {
+        "id": 100,
+        "name": "kirimaru",
+        "children":  [
+          {
+            "id": 101
+          },
+          {
+            "id": 102
+          }
+        ]
+      }
+    }
+    """
+    expected = """
+    {
+      "parent": {
+        "name": "kirimaru",
+        "id": 100,
+        "children":  [
+          {
+            "id": 102
+          },
+          {
+            "id": 101
+          }
+        ]
+      }
+    }
+    """
+    print("***********")
+    print(json.loads(actual)["parent"]["chidren"])
+    print(json.loads(actual)["parent"]["chidren"].items())
+    # print(json.loads(actual)["children"].items())
+
+    assert sorted(json.loads(actual).items()) == sorted(json.loads(expected).items())
