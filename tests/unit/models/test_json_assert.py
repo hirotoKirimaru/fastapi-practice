fb209c661d0aac2a70dd66a5e8410a78633602c9
水上 皓登 <nainaistar@gmail.com>
2022-05-04 13:25:03 +0900

    json比較？

---
+++ tests/unit/models/test_json_assert.py
@@ -0,0 +1,37 @@
+import pytest
+import json
+
+
+def test_01():
+    actual = """
+    {
+      "parent": {
+        "id": 100,
+        "children":  [
+          {
+            "id": 101
+          },
+          {
+            "id": 102
+          }
+        ]
+      }
+    }
+    """
+    expected = """
+    {
+      "parent": {
+        "id": 100,
+        "children":  [
+          {
+            "id": 101
+          },
+          {
+            "id": 102
+          }
+        ]
+      }
+    }
+    """
+
+    assert json.loads(actual) == json.loads(expected)
