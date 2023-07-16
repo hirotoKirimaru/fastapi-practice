decc25e1c91c4f230eb8d27ab8f7acab10cce3de
水上 皓登 <nainaistar@gmail.com>
2023-07-16 10:54:35 +0000

    検索してテスト

--- tests/unit/cruds/test_user.py
+++ tests/unit/cruds/test_user.py
@@ -1,17 +1,18 @@
-from functools import wraps
-from sqlalchemy.sql import text
+from sqlalchemy import create_engine, select, text
 from sqlalchemy.ext.asyncio import AsyncSession
 import pytest
+from src.models.user import User
 
 @pytest.mark.asyncio
 async def test_01(db: AsyncSession) -> None:
     user1 = User(id=1, name="11", email="a@example.com", organization="開発部")
-    await db.add(user1)
+    
+    db.add(user1)
     await db.commit();
 
     query: select = select(User)
 
-    actual = await db.query(query).first()
+    actual = (await db.execute(query)).scalars().all()
 
-    assert user1.id == actual.id
+    assert user1.id == actual[0].id
 
