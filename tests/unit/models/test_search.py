f7608c38633e07f292ff69423ec0c65ffd75054c
水上 皓登 <nainaistar@gmail.com>
2023-09-14 00:47:20 +0000

    WIP

--- tests/unit/models/test_search.py
+++ tests/unit/models/test_search.py
@@ -45,3 +45,53 @@ def test_02():
         processingTime += end - start
 
     print(processingTime / 10)
+
+import datetime
+
+class User:
+    def __init__(self, user_id, email = None, name = None, birthday=None):
+        self.id = id
+        self.email = email
+        self.name = name
+        self.birthday = birthday
+
+    user_id: str
+    email: str
+    name: str
+    birthday: datetime
+
+@pytest.mark.skipif(False, reason="ちょっと重いからスキップさせる")
+def test_03():
+    print("333333333")
+    processingTime: float = 0
+    for y in range(10):
+        start = time.perf_counter()
+
+        a = [User(user_id=str(x)) for x in range(100000)]
+
+        for x in range(100):
+            [x for x in a if x.id == random.randrange(100000)]
+
+        end = time.perf_counter()
+        processingTime += end - start
+
+    print(processingTime / 10)
+
+
+@pytest.mark.skipif(False, reason="ちょっと重いからスキップさせる")
+def test_04():
+    print("4444444444")
+    processingTime: float = 0
+    for y in range(10):
+        start = time.perf_counter()
+
+        a = [User(user_id=str(x)) for x in range(100000)]
+        b: Dict[int, int] = {x.id: x for x in a}
+
+        for x in range(100):
+            b.get(random.randrange(100000))
+
+        end = time.perf_counter()
+        processingTime += end - start
+
+    print(processingTime / 10)
