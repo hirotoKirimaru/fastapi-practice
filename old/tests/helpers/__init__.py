6ca26433e347e5634d7515001205fbcfde2ad810
水上 皓登 <nainaistar@gmail.com>
2023-11-19 22:43:39 +0900

    del: poetryのファイルを削除する

--- old/tests/helpers/__init__.py
+++
@@ -1,1 +0,0 @@
-from .custom_assert import CustomAssert
--- old/tests/scenario/test_healthcheck.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import starlette.status
-
-
-@pytest.mark.asyncio
-async def test_response_json(async_client):
-    response = await async_client.get("/health/")
-    assert response.status_code == starlette.status.HTTP_200_OK
-    assert response.json() == dict(Hello="World")
