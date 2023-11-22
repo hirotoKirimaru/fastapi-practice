756935a116d65cede1f41e4eda05fef6cb4dac11
水上 皓登 <nainaistar@gmail.com>
2023-11-22 21:59:28 +0900

    feat: 年齢を計算する.

--- src/models/user.py
+++ src/models/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+import datetime
+
+from sqlalchemy import Column, Integer, String, Date
 from src.db import Base
 
 
@@ -9,3 +11,11 @@ class User(Base):
     name = Column(String(1024))
     email = Column(String(1024))
     organization = Column(String(1024))
+    birth_day = Column(Date)
+
+    def age(self, now: datetime.date) -> int:
+        """
+        年齢を計算する.
+        """
+        return now.year - self.birth_day.year - ((now.month, now.day) < (self.birth_day.month, self.birth_day.day))
+
--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -1,5 +1,8 @@
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import date
+
+import src.models.user
 
 
 class TestUser:
@@ -10,3 +13,16 @@ class TestUser:
         # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         print(*[x for x in range(10)])
         # 0 1 2 3 4 5 6 7 8 9
+
+class TestAge:
+    NOW: date = date(2023, 11, 20)
+    @pytest.mark.parametrize("input_birth_day, expected_age", [
+        (date(2023, 11, 20), 0),
+        (date(2023, 11, 19), 0),
+        (date(2022, 11, 21), 0),
+        (date(2022, 11, 20), 1),
+        (date(2022, 11, 19), 1),
+        (date(1992, 2, 4), 31)
+    ])
+    def test_one(self, input_birth_day: date, expected_age: int):
+        assert src.models.user.User(birth_day=input_birth_day).age(now=self.NOW) == expected_age
