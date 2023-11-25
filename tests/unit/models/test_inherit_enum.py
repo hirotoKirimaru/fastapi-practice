74b824acfb82eaffb05b8ab26d47502e40238342
水上 皓登 <nainaistar@gmail.com>
2023-11-25 23:16:58 +0900

    feat: intを継承したenumもいい

---
+++ tests/unit/models/test_inherit_enum.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class CsvHeaderColumn(int, Enum):
+    """
+    intを継承しているとvalueにアクセスしなくてもアクセスできる
+
+    """
+    ID = 1
+    NAME = 2
+    EMAIL = 3
+    ADMIN_FLG = 4
+
+
+class Test:
+    def test_normal(self):
+        # 正しくはこちらのアクセス
+        assert CsvHeaderColumn.ID.value == 1
+
+        assert CsvHeaderColumn.ID == 1
+        assert CsvHeaderColumn.NAME == 2
+        assert CsvHeaderColumn.EMAIL == 3
+        assert CsvHeaderColumn.ADMIN_FLG == 4
+        assert len(CsvHeaderColumn) == 4, f"定義している数({4})が一致していること"
+
+
+
+
