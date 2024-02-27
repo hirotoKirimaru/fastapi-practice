5df892dd0d0f92f1cd60597e51ef64a623deae39
hirotoKirimaru <nainaistar@gmail.com>
2024-02-27 23:37:01 +0900

    feat: PyJwtで追加

---
+++ tests/unit/models/test_jwt.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+import jwt
+from datetime import datetime
+
+
+class JwtToken(BaseModel):
+    id: int
+    exp: int | datetime
+    iss: str
+
+
+async def test_expected_same_token():
+    KEY = "SECRET"
+    ALGORITHM = "HS256"
+    json_token = {"id": 1, "exp": 1710152710, "iss": "kirimaru"}
+
+    pydantic_token = JwtToken(**json_token).model_dump()
+
+    # NOTE: JSON のキーの順番が違ってもTrue になる
+    assert json_token == pydantic_token
+
+    # NOTE: JWTの payload の順番が違ったら別のトークンになるのでソートしたら True になる
+    json_encode = jwt.encode(dict(sorted(json_token.items())), KEY, algorithm=ALGORITHM)
+    pydantic_encode = jwt.encode(
+        dict(sorted(pydantic_token.items())), KEY, algorithm=ALGORITHM
+    )
+    assert json_encode == pydantic_encode
+
+    # NOTE: ソートしなくても別のトークンから同じJSONに戻せることのチェック
+    json_encode = jwt.encode(json_token, KEY, algorithm=ALGORITHM)
+    pydantic_encode = jwt.encode(pydantic_token, KEY, algorithm=ALGORITHM)
+    json_decode = jwt.decode(json_encode, KEY, algorithms=ALGORITHM)
+    pydantic_decode = jwt.decode(pydantic_encode, KEY, algorithms=ALGORITHM)
+
+    assert json_decode == pydantic_decode
