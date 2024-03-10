8e8252ea5fdc215233d336ceb8179adf657aca1b
hirotoKirimaru <nainaistar@gmail.com>
2024-03-10 21:06:26 +0900

    objectを継承していることの確認

---
+++ tests/unit/models/test_object.py
@@ -0,0 +1,23 @@
+class Hoge:
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name
+
+class Fuga(object):
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name
+
+class TestHogeFuga:
+    async def test_01(self):
+        hoge = Hoge(name = "aiueo")
+        assert hoge.name == "aiueo"
+        assert type(hoge) is Hoge
+        assert issubclass(Hoge, object)
+
+        fuga = Fuga(name = "aiueo")
+        assert fuga.name == "aiueo"
+        assert type(fuga) is Fuga
+        assert issubclass(Fuga, object)
+
+        assert type(hoge) is not type(fuga)
