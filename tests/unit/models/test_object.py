1bb703e65e31a5a687757b904f0830ef7f49272e
hirotoKirimaru <nainaistar@gmail.com>
2024-03-23 13:24:46 +0900

    autoformat

--- tests/unit/models/test_object.py
+++ tests/unit/models/test_object.py
@@ -3,19 +3,21 @@ class Hoge:
         super().__init__()
         self.name = name
 
+
 class Fuga(object):
     def __init__(self, name: str):
         super().__init__()
         self.name = name
 
+
 class TestHogeFuga:
     async def test_01(self):
-        hoge = Hoge(name = "aiueo")
+        hoge = Hoge(name="aiueo")
         assert hoge.name == "aiueo"
         assert type(hoge) is Hoge
         assert issubclass(Hoge, object)
 
-        fuga = Fuga(name = "aiueo")
+        fuga = Fuga(name="aiueo")
         assert fuga.name == "aiueo"
         assert type(fuga) is Fuga
         assert issubclass(Fuga, object)
