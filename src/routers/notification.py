02017b2e891bd1f9095bb635c8b2812e25cdfe6c
hirotoKirimaru <nainaistar@gmail.com>
2024-04-21 21:52:14 +0900

    feat: backgroundTaskにパラメータを渡す

--- src/routers/notification.py
+++ src/routers/notification.py
@@ -1,14 +1,19 @@
 from typing import Any
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from datetime import datetime
 import src.schemas.notification
 from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from src.db import get_reader_db
 from src.schemas.notification import Notification as schema_notification
+from src.helper.datetime_resolver import DatetimeResolver
 
 router = APIRouter()
 
-async def read():
+async def read(db, read_time: datetime):
+    # TODO: あれ、Serializeしなくても渡せる…？
+    print(db)
+    print(read_time)
     print("XXXXXXXXXXXXXXXXX")
 
 @router.post("/unread", response_model=schema_notification)
@@ -16,5 +21,5 @@ async def read_unread_notifications(
     tasks: BackgroundTasks,
     db: AsyncSession = Depends(get_reader_db),
 ) -> Any:
-    tasks.add_task(read)
+    tasks.add_task(read, db=db, read_time=DatetimeResolver.now())
     return schema_notification(description= "123")
