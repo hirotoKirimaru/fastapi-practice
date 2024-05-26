ce01201e350ee60fd9e77e8afeac8a1b139b426e
hirotoKirimaru <nainaistar@gmail.com>
2024-05-26 23:09:57 +0900

    feat: MissingGreenletの例追加

--- tests/unit/cruds/test_user.py
+++ tests/unit/cruds/test_user.py
@@ -93,11 +93,16 @@ class TestUserRelationShip:
         user1 = User(id=1, name="11", email="a@example.com", organization_id=1)
         user2 = User(id=2, name="22", email="b@example.com", organization_id=1)
         user3 = User(id=3, name="33", email="c@example.com", organization_id=1)
+        user_ng = User(id=100, name="100", email="100@example.com", organization_id=2)
         organization_1 = Organization(id=1)
+        organization_2 = Organization(id=2)
         db.add(user1)
         db.add(user2)
         db.add(user3)
         db.add(organization_1)
+
+        db.add(user_ng)
+        db.add(organization_2)
         await db.commit()
 
         query: Select = (select(User)
@@ -110,16 +115,22 @@ class TestUserRelationShip:
         # 実行時にLeft outer joinがいいか、別でID検索したほうが総合的に早いか…？
         # https://docs.sqlalchemy.org/en/20/orm/relationship_api.html#sqlalchemy.orm.relationship.params.lazy
         query: Select = (select(User)
-                         .options(joinedload(User.organization))
                          .where(User.id == 2))
         result = (await db.execute(query)).scalars().first()
         assert result.organization is not None
 
         query: Select = (select(User)
-                         .options(joinedload(User.organization))
                          .where(User.id == 3))
         result = (await db.execute(query)).scalars().first()
         assert result.organization is not None
+
+        # NOTE: organization2, 3, 4 を定義していなければ、ここでエラーになる
+        query: Select = (select(User)
+                         .where(User.id == 100))
+        result = (await db.execute(query)).scalars().first()
+        assert result.organization is not None
+        # with pytest.raises(MissingGreenlet) as e:
+        #     _ = result.organization
 
     async def test_immediate(self, db: AsyncSession):
         """
