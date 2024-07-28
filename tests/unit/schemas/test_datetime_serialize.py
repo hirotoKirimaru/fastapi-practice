7a2d39de531c698e5cfca884d258dc037780b829
hirotoMizukami <nainaistar@gmail.com>
2024-07-28 18:09:47 +0900

    デシリアライズも確認する

--- tests/unit/schemas/test_datetime_serialize.py
+++ tests/unit/schemas/test_datetime_serialize.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
-from pydantic import BaseModel, field_serializer, PlainSerializer
+from pydantic import BaseModel, field_serializer, PlainSerializer, PlainValidator
 import json
 from typing import Annotated
 from pydantic import BaseModel, PlainSerializer
@@ -26,7 +26,8 @@ class TestFieldSerializer:
 
 CustomDatetime = Annotated[
     datetime,
-    PlainSerializer(lambda dt: dt.strftime('%Y-%m-%d %H:%M:%S'))
+    PlainSerializer(lambda dt: dt.strftime('%Y-%m-%d %H:%M:%S')),
+    # PlainValidator(lambda dt: datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')), # str -> datetime, datetime -> strが混ざるのでNG。
 ]
 
 class _TestModel2(BaseModel):
@@ -34,7 +35,7 @@ class _TestModel2(BaseModel):
     normal_dt: datetime
 
 class TestCustomDatetime:
-    async def test_01(self):
+    async def test_serialize(self):
         dt = datetime(2024, 12, 1, 2, 3, 4, tzinfo=ZoneInfo('UTC'))
         model = _TestModel2(dt=dt, normal_dt=dt)
         actual_str = model.model_dump_json()
@@ -42,3 +43,15 @@ class TestCustomDatetime:
 
         assert actual_dict.get('dt') == "2024-12-01 02:03:04"
         assert actual_dict.get('normal_dt') == "2024-12-01T02:03:04Z"
+
+    async def test_deserialize(self):
+        # GIVEN
+        dt = datetime(2024, 12, 1, 2, 3, 4, tzinfo=ZoneInfo('UTC'))
+        model = _TestModel2(dt=dt, normal_dt=dt)
+        actual_str = model.model_dump_json()
+
+        # WHEN
+        actual = _TestModel2.model_validate_json(actual_str)
+        assert actual.dt == dt.replace(tzinfo=None) # シリアライズ時にTZが削除されている
+        assert actual.normal_dt == dt
+
