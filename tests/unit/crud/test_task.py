4be60932b463536e621123c37518741dd099efa0
hirotoMizukami <nainaistar@gmail.com>
2024-08-25 23:27:38 +0900

    検証しようとしてできなかった

--- tests/unit/crud/test_task.py
+++ tests/unit/crud/test_task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import and_, select, func
+from sqlalchemy import and_, select, func, text
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
@@ -6,7 +6,7 @@ from src.models.task import Task, Done
 
 
 class TestTaskCascade:
-    async def test_01(self, db: AsyncSession) -> None:
+    async def test_delete_at_app(self, db: AsyncSession) -> None:
         """
         Taskを削除したときに子テーブルのDoneも削除すること。
         """
@@ -41,3 +41,37 @@ class TestTaskCascade:
         actual = (await db.execute(query)).scalars().all()
         assert {2, 3} == {x.id for x in actual}
 
+    # NOTE: アプリ上で実行するときはこれは動かない
+    async def _test_delete_at_db(self, db: AsyncSession) -> None:
+        """
+        Taskを削除したときに子テーブルのDoneも削除すること。
+        """
+        task1 = Task(id=1, title="Test1")
+        task2 = Task(id=2, title="Test1")
+        task3 = Task(id=3, title="Test1")
+        db.add_all([task1, task2, task3])
+
+        done1 = Done(id=1)
+        done2 = Done(id=2)
+        done3 = Done(id=3)
+        db.add(done1)
+        db.add(done2)
+        db.add(done3)
+        await db.commit()
+
+        # WHEN
+        print("XXXXXX")
+        delete_statement = text("DELETE FROM tasks WHERE id = :id")
+        await db.execute(delete_statement, {"id": task1.id})
+        await db.commit()
+        print("XXXXXX")
+
+        # THEN
+        query: select = select(Task).options(joinedload(Task.done))
+        actual = (await db.execute(query)).scalars().all()
+        assert {2, 3} == {x.id for x in actual}
+
+        # THEN
+        query: select = select(Done)
+        actual = (await db.execute(query)).scalars().all()
+        assert {2, 3} == {x.id for x in actual}
