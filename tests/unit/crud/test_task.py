82a1c6ebc5f8fe73dddfe6f390daa09006579a56
hirotoMizukami <nainaistar@gmail.com>
2024-08-31 18:26:09 +0900

    deleteクエリを発行

--- tests/unit/crud/test_task.py
+++ tests/unit/crud/test_task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import and_, select, func, text
+from sqlalchemy import and_, select, func, text, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
@@ -40,6 +40,43 @@ class TestTaskCascade:
         query: select = select(Done)
         actual = (await db.execute(query)).scalars().all()
         assert {2, 3} == {x.id for x in actual}
+
+    async def test_delete_by_delete_query(self, db: AsyncSession) -> None:
+        """
+        直接Taskをdeleteクエリで削除しても、子のDoneは削除されないこと。
+        """
+        task1 = Task(id=1, title="Test1")
+        task2 = Task(id=2, title="Test1")
+        task3 = Task(id=3, title="Test1")
+        db.add(task1)
+        db.add(task2)
+        db.add(task3)
+
+        done1 = Done(id=1)
+        done2 = Done(id=2)
+        done3 = Done(id=3)
+        db.add(done1)
+        db.add(done2)
+        db.add(done3)
+        await db.commit()
+
+        # WHEN
+        query = delete(Task).where(Task.id == 1)
+        _ = await db.execute(query)
+        await db.commit()
+        print("XXXXXX")
+
+        # THEN
+        query: select = select(Task).options(joinedload(Task.done))
+        actual = (await db.execute(query)).scalars().all()
+        assert {2, 3} == {x.id for x in actual}
+
+        # THEN
+        query: select = select(Done)
+        actual = (await db.execute(query)).scalars().all()
+        assert {1, 2, 3} == {x.id for x in actual}
+
+
 
     # NOTE: アプリ上で実行するときはこれは動かない
     async def _test_delete_at_db(self, db: AsyncSession) -> None:
