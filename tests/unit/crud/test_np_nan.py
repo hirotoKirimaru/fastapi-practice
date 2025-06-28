756e424e9f163323be28d31e4287af21d01170d2
hirotoMizukami <nainaistar@gmail.com>
2025-06-28 13:21:25 +0900

    add test

---
+++ tests/unit/crud/test_np_nan.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sqlalchemy import and_, func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.models.user import User
+
+
+class TestNpNan:
+
+    async def test_01(self, db: AsyncSession) -> None:
+        """
+        TODO: 昔はこれでエラーになることがあった
+        """
+        query = select(User).where(User.id.in_([np.nan]))
+        actual = (await db.execute(query)).scalars().all()
+
+        assert len(actual) == 0
+
